6dd4f268fbbf93e48cbdd732209b8052a9bb12a8
Emmanuelito18 <emmanuelito18yt@gmail.com>
2024-07-15 23:55:10 -0500

    -Cambios en conectarSQLite.java *Se añadieron variables globales para la coenxión a la base de datos líneas 16-17 *Se añadio método para realizar la conexión a la base de dato SQLite líneas 18-32

--- src/clases/conectarSQLite.java
+++ src/clases/conectarSQLite.java
@@ -13,5 +13,21 @@ import java.sql.SQLException;
  * @author Emmanuel
  */
 public class conectarSQLite {
-    
+    String url="jdbc:sqlite:src\\baseDatos\\pruebaSQLite.db";//Establece el url de la base de datos
+    Connection connect;//crea una variable de tipo Connection, se utilizará para realizar la conexión
+    //<editor-fold defaultstate="collapsed" desc="conexion de la base de datos">
+    /**
+     * Este método se utiliza para realizar la conexión a la base de datos SQLite
+     */
+    public void conectarInterna(){
+        try{
+            connect=DriverManager.getConnection(url);
+            if(connect!=null){
+                System.out.println("Estoy conectado a la base de datos");
+            }
+        }catch(SQLException error){
+            error.printStackTrace();
+        }
+    }
+    //</editor-fold>
 }
