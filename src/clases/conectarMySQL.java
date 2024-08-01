ae90c5e52f70f34824d7f2aff1b455ed4f3b432a
Emmanuelito18 <emmanuelito18yt@gmail.com>
2024-07-31 21:17:34 -0500

    -Cambios en conectarMySQL.java *Se añadieron espacios para los métodos para la gestion de la base de datos

--- src/clases/conectarMySQL.java
+++ src/clases/conectarMySQL.java
@@ -1,10 +1,40 @@
 package clases;
 
+//<editor-fold defaultstate="collapsed" desc="Librerias para la base de datos MySQL">
+
+//</editor-fold>
+
 /**
  *Esta clase se utiliza para manejar la conexión y operaciones de la 
  * base de datos MySQL
  * @author Emmanuel
  */
 public class conectarMySQL {
+    //<editor-fold defaultstate="collapsed" desc="Conexión de la base de datos">
     
+    //</editor-fold>
+    
+    //<editor-fold defaultstate="collapsed" desc="Cierra la conexión a la base de datos">
+    
+    //</editor-fold>
+    
+    //<editor-fold defaultstate="collapsed" desc="Acciones de la base de dato">
+    
+    //<editor-fold defaultstate="collapsed" desc="Mete información a la base de datos">
+    
+    //</editor-fold>
+    
+    //<editor-fold defaultstate="collapsed" desc="Muesta información de la base de datos">
+    
+    //</editor-fold>
+    
+    //<editor-fold defaultstate="collapsed" desc="Elimina infomarción de la base de datos">
+    
+    //</editor-fold>
+    
+    //<editor-fold defaultstate="collapsed" desc="Actualiza información de la base de datos">
+    
+    //</editor-fold>
+    
+    //</editor-fold>
 }
