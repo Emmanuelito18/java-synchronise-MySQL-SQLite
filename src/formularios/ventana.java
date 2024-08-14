0409653e15dabf72979c1767be0560c3607b064f
Emmanuelito18 <emmanuelito18yt@gmail.com>
2024-08-14 01:25:48 -0500

    -Cambios en ventana.java *Se modifico el modelo de la tabla tblMySQL para que coincida con la estructura de la base de datos

--- src/formularios/ventana.java
+++ src/formularios/ventana.java
@@ -87,15 +87,23 @@ public class ventana extends javax.swing.JFrame {
 
         tblMysql.setModel(new javax.swing.table.DefaultTableModel(
             new Object [][] {
-                {null, null, null, null},
-                {null, null, null, null},
-                {null, null, null, null},
-                {null, null, null, null}
+                {null, null, null, null, null},
+                {null, null, null, null, null},
+                {null, null, null, null, null},
+                {null, null, null, null, null}
             },
             new String [] {
-                "Title 1", "Title 2", "Title 3", "Title 4"
+                "ID", "Nombre", "Plataforma", "Fecha cración", "Fecha actualización"
             }
-        ));
+        ) {
+            boolean[] canEdit = new boolean [] {
+                false, false, false, false, false
+            };
+
+            public boolean isCellEditable(int rowIndex, int columnIndex) {
+                return canEdit [columnIndex];
+            }
+        });
         jScrollPane1.setViewportView(tblMysql);
 
         btnConectarMysql.setText("Conectar");
